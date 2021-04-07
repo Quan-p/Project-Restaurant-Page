7a007ad1a43415312be40262fd13c2ba704c618e
Quan <quanp155@gmail.com>
2021-04-07 15:18:27 -0500

    Tested removing title from html file and adding it strictly using javascript

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,4 @@
-console.log("hello");
+var head = document.createElement("h1");
+var headContent = document.createTextNode("Fake Restaurant");
+head.appendChild(headContent);
+document.getElementById('content').appendChild(head);
