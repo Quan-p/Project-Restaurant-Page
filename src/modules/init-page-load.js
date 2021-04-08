4d42bcdb3f9578da088c07d83f662f6e2618793a
Quan-p <quanp155@gmail.com>
2021-04-08 17:12:06 -0500

    Created new directory for modules

---
+++ src/modules/init-page-load.js
@@ -0,0 +1,11 @@
+function createHeader() {
+    var head = document.createElement('h1');
+    var headContent = document.createTextNode('Fake Restaurant');
+    head.appendChild(headContent);
+    document.getElementById('content').appendChild(head);
+    return head;
+}
+var homePic = document.createElement('img');
+homePic.src = '/public/images/homePic.jpg';
+document.getElementById('content').appendChild(homePic);
+homePic.classList.add("mainImg");
