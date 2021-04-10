73af67a9d3c51dbd75feaaa079004cece4cea6b0
Quan-p <quanp155@gmail.com>
2021-04-09 21:35:34 -0500

    Changed the createMainImg function to pass in the picture source on page load

--- src/modules/init-page-load.js
+++ src/modules/init-page-load.js
@@ -1,26 +1,27 @@
 function createTitle(id, text) {
     var header = document.createElement('header');
     header.setAttribute('id', id);
-    const title = document.createElement('h1');
+    var title = document.createElement('h1');
     title.textContent = text;
     header.appendChild(title)
     return header;
 }
 
-function createMainImg() {
-    var homePic = document.createElement('img');
-    return homePic;
+function createMainImg(src, alt) {
+    var mainImg = document.createElement('img');
+    mainImg.classList.add('homeFood');
+    mainImg.setAttribute('src', src);
+    mainImg.setAttribute('alt', alt);
+    content.appendChild(mainImg);
+    return mainImg;
 }
 
 function homePageLoad() {
     const content = document.getElementById('content');
 
     var header = createTitle('header', 'Fake Restaurant');
-    //title.appendChild(titleContent);
     content.appendChild(header);
 
-    homePic.src = '/public/images/homePic.jpg';
-    content.appendChild(homePic);
-    homePic.classList.add("mainImg");
+    createMainImg('/public/images/homePic.jpg', 'Restaurant food table');
 }
 export default homePageLoad;
