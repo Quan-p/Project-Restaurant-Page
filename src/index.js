f0c104a115680f2848ba79f1fb6028374ff85047
Quan-p <quanp155@gmail.com>
2021-04-13 16:02:47 -0500

    Imported functions from modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import homePageLoad from './modules/init-page-load.js';
+import menuLoad from '.modules/menu.js';
+import contactLoad from '.modules/contact.js';
 
 function init() {
     homePageLoad();
 }
+
+
 
 init();
