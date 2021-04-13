fb1752dfbe1632fe123f6228c4a365c9f079058b
Quan-p <quanp155@gmail.com>
2021-04-13 16:09:21 -0500

    Created function to add onclick event listeners in order to load the different modules for each corresponding tab

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import homePageLoad from './modules/init-page-load.js';
 import menuLoad from '.modules/menu.js';
 import contactLoad from '.modules/contact.js';
 
-function init() {
-    homePageLoad();
+function navEvents() {
+    var homeBtn = document.getElementById('home');
+    var menuBtn = document.getElementById('menu');
+    var contactBtn = document.getElementById('contact');
+
+    homeBtn.addEventListener('click', homePageLoad);
+    menuBtn.addEventListener('click', menuLoad);
+    contactBtn.addEventListener('click', contactLoad);
 }
 
-
+function init() {
+    homePageLoad();
+    navEvents();
+}
 
 init();
