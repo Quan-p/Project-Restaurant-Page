9e8881c2f020869c216445e038d92c389842b3c9
Quan-p <quanp155@gmail.com>
2021-04-13 21:57:02 -0500

    Added the elements for different tabs to corresponding variable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import homePageLoad from './modules/init-page-load.js';
-import menuLoad from '.modules/menu.js';
-import contactLoad from '.modules/contact.js';
+import menuLoad from './modules/menu.js';
+import contactLoad from './modules/contact.js';
 
 function navEvents() {
     var homeBtn = document.getElementById('home');
