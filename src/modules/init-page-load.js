a66e0424bc3b3f52bb20f8ebb3a274d30a0d4b23
Quan-p <quanp155@gmail.com>
2021-04-16 18:03:05 -0500

    Deleted redundant functions on page load file that were taken from the home page file

--- src/modules/init-page-load.js
+++ src/modules/init-page-load.js
@@ -15,35 +15,6 @@ function createTitle(id, text) {
     return header;
 }
 
-function createMainImg(src, alt) {
-    var mainImg = document.createElement('img');
-    mainImg.setAttribute('id', 'tableImg');
-    mainImg.classList.add('homeFood');
-    mainImg.setAttribute('src', src);
-    mainImg.setAttribute('alt', alt);
-    tabContent.appendChild(mainImg);
-    return mainImg;
-}
-
-function createSection() {
-    var description = document.createElement('section');
-    description.classList.add('section');
-    description.setAttribute('id', 'about-section')
-    
-    var title = document.createElement('h2')
-    title.classList.add('section-title');
-    title.textContent = 'About';
-
-    description.appendChild(title);
-
-    var content = document.createElement('p');
-    content.classList.add('section-description');
-    content.textContent = 'We offer the best food from all over Middle-Earth, allowing you to escape into the greatest fictional world. Lorem ipsum dolor sitamet consectetur adipisicing elit. Animi in, neque, dignissimosperspiciatis illo deserunt tetur adipisicing elit.'
-
-    description.appendChild(content);
-    
-    return description;
-}
 
 function tabCreate(id, text) {
     var btn = document.createElement('button');
@@ -80,9 +51,5 @@ function pageLoad() {
     
     mainTab();
     
-    createMainImg('https://pbs.twimg.com/media/DAuME7uVwAAYBCK.jpg', 'Dining tables with food');
-
-    const aboutSection = createSection();
-    tabContent.appendChild(aboutSection);
 }
 export default pageLoad;
