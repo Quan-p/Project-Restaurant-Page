a7c0b3ac32ea6dd6c5b213ee5fa2a89c0bd4e7b4
Quan-p <quanp155@gmail.com>
2021-04-16 12:48:12 -0500

    Added the set button active function across all 3 tabs

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,6 +1,16 @@
 function contactLoad() {
     document.getElementById('tabContent').innerHTML = '';
 
+    setBtnActive('contact');
+
+}
+
+function setBtnActive(id) {
+    var activeBtn = document.querySelector('.tabs.active');
+    if (activeBtn) activeBtn.classList.remove('active');
+
+    var homeBtn = document.getElementById(id);
+    homeBtn.classList.add('active');
 }
 
 export default contactLoad;
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,6 +1,16 @@
 function menuLoad() {
     document.getElementById('tabContent').innerHTML = '';
 
+    setBtnActive('menu');
+
+}
+
+function setBtnActive(id) {
+    var activeBtn = document.querySelector('.tabs.active');
+    if (activeBtn) activeBtn.classList.remove('active');
+
+    var homeBtn = document.getElementById(id);
+    homeBtn.classList.add('active');
 }
 
 export default menuLoad;
