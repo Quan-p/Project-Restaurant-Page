31a1729737ac2f7a60f086a3247efeab6c4610f4
Quan-p <quanp155@gmail.com>
2021-04-21 16:09:24 -0500

    Created function that forms a menu item after passing through a src for the image, description, etc

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,9 +1,36 @@
+function createFood(src, alt, text, desc) {
+    var food = document.createElement('div')
+    food.classList.add('food');
+    
+    var img = document.createElement('img');
+    img.setAttribute('src'. src);
+    img.setAttribute('alt'. alt);
+
+    var description = document.createElement('div');
+    description.classList.add('description');
+
+    const foodName = document.createElement('h2')
+    foodName.textContent = text;
+
+    const foodDesc = document.createElement('p')
+    foodDesc.textContent = desc;
+
+    description.appendChild(foodName);
+    description.appendChild(foodDesc);
+
+    food.appendChild(img);
+    food.appendChild(description);
+    return food;
+}
+
+
 function menuLoad() {
     var mainContent = document.getElementById('tab-content');
-
     mainContent.textContent = '';
-    setBtnActive('menu');
 
+    
+    
+    setBtnActive('menu');
 }
 
 function setBtnActive(id) {
